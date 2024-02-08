e5c7dead35f452e0ff18580a4a0d6bb55e3469e1
Srinithya-138 <srinithyam2001@gmail.com>
2024-02-08 16:16:44 +0530

    tdy

---
+++ reactsampleproject/firstreact/src/App.js
@@ -0,0 +1,15 @@
+import FIRST from "./reacttyrial/first"
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <button class="btn btn-secondary m-5 ">secondary button</button>
+      <FIRST/>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactsampleproject/firstreact/src/reacttyrial/first.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const first = () => {
+  return (
+    <div class="p-4">
+      <p>lorem hiwfhetherfjhrfkldnsdf;jsrdghsejo</p>
+     <button type="button" class="btn btn-primary">hi</button>
+    </div>
+  )
+}
+
+export default first
