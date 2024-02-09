add55ddb11a617d400cc04aac585916236d24db9
Srinithya-138 <srinithyam2001@gmail.com>
2024-02-09 16:41:10 +0530

    tdy

--- website-tutorial/src/App.js
+++ website-tutorial/src/App.js
@@ -2,15 +2,22 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from './pages/Home'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Menu from "./pages/Menu";
+import About from "./pages/about";
+import Contact from "./pages/Contact";
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact component={Home}/>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/menu" exact element={<Menu/>}/>
+          <Route path="/about" exact element={<About/>}/>
+          <Route path="/contact" exact element={<Contact/>}/>
         </Routes>
       </Router>
+     
     </div>
   );
 }
--- website-tutorial/src/components/Navbar.js
+++ website-tutorial/src/components/Navbar.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
 import Logo from "../assets/pizzaLogo.png";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import ReorderIcon from "@mui/icons-material/Reorder";
 
 const Navbar = () => {
+  const[openLinks,setOpenLinks]=useState(false)
+  const toggleNavbar=()=>{
+    setOpenLinks(!openLinks);
+
+  }
   return (
     <div className="navbar">
-      <div className="leftSide">
+      <div className="leftSide" id={openLinks ?"open":"close"}>
         <img src={Logo} alt="pizza" />
+        <div className="hiddenLinks">
+        <Link to="/">Home</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        </div>
       </div>
       <div className="rightSide">
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
-        <button>
+        <button onClick={toggleNavbar}>
           <ReorderIcon />
         </button>
       </div>
