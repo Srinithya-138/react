c107a4e8fce5e52f7e4dc24a195f6b7ad9bf8f19
Srinithya-138 <srinithyam2001@gmail.com>
2024-02-29 16:42:43 +0530

    tdy

--- real-estate/src/hooks/useGetUserInfo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const useGetUserInfo = () => {
-  const { name, userId, isAuth } = JSON.parse(localStorage.getItem("auth"));
-  return <div>name,userId,isAuth;</div>;
-};
-
-export default useGetUserInfo;
