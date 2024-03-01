20bb25a14f1f7ac85d094f23214b7d80b0231cef
Srinithya-138 <srinithyam2001@gmail.com>
2024-03-01 16:47:12 +0530

    tdy

---
+++ react-redux-tutorial/src/redux/reducer/productReducer.js
@@ -0,0 +1,19 @@
+import { ActionTypes } from "../constants/action-types"
+
+
+const initialState={
+    products:[{
+        id:1,
+        titile:"myname",
+        category:"programmeer"
+    }]
+}
+export const productReducer=(state,{type,payload})=>{
+    switch (type ){
+        case ActionTypes.SET_PRODUCTS :
+            return state
+        default:
+            return state;
+    }
+
+}
