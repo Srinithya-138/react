6cd4f72303df9f0b29ac4e5d31f86518e094d2f6
Srinithya-138 <srinithyam2001@gmail.com>
2024-03-07 16:04:38 +0530

    tdy

---
+++ react-hooks/src/use-state/Counter.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount(prevState =>prevState+1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Click me </button>
+      <p>Current value of count is {count}</p>
+      <button onClick={() => increaseFive()}>click me to add 5</button>
+    </div>
+  );
+};
+
+export default Counter;
